0a67ee99d97e769ec2988121dd9e2c573523c5b7
detamos <deepanshu_tyagi@students.iitmandi.ac.in>
2017-05-02 21:19:27 +0530

    Driver programs updated

--- src/directed.cpp
+++ src/directed.cpp
@@ -1,35 +1,163 @@
 #include <iostream>
+#include <fstream>
+#include <string.h>
+#include <cstdlib>
 #include "DirectedGraph.hpp"
 #include "../lib/DirectedGraph.cpp"
 
 using namespace cs202;
 using namespace std;
 
-void print(const int &a)
+int getLines(char *);
+
+void print(const int &i)
 {
-	cout<<a<<" ";
+	cout<<i<<" ";
 }
 
-int main()
+int main(int argc,char *argv[])
 {
-	DirectedGraph a(5,'l');
-	DirectedGraph b(5,'m');
-	a.add(0,1); b.add(0,1);
-	a.add(0,2); b.add(0,2);
-	a.add(3,4); b.add(3,4);
-	a.add(1,3); b.add(1,3);
-	a.add(2,4); b.add(2,4);
-	a.print(); 
-	cout<<"BFS : ";
-	a.bfs(&print);
-	cout<<"\tDFS : ";
-	a.dfs(&print);
-	cout<<endl;
-	b.print();
-	cout<<"BFS : ";
-	b.bfs(&print);
-	cout<<"\tDFS : ";
-	b.dfs(&print);
-	cout<<endl;	
+	if(argc < 2)
+	{
+		cerr<<"Wrong number of arguments\n";
+		return 0;
+	}
+
+	int size;
+	char ch;
+
+	if(argc == 3)
+	{
+		size = getLines(argv[1]);
+		ch = argv[2][0];
+	}
+	else
+	{
+		cin>>size;
+		ch = argv[1][0];
+	}
+
+	int mat[size][size];
+
+	if(argc == 3)
+	{
+		ifstream inFile;
+		inFile.open(argv[1]);
+
+		for(int i=0;i<size;i++)
+		{
+			for(int j=0;j<size;j++)
+			{
+				inFile>>mat[i][j];
+			}
+		}
+	}
+	else
+	{
+		for(int i=0;i<size;i++)
+		{
+			for(int j=0;j<size;j++)
+			{
+				cin>>mat[i][j];
+			}
+		}
+	}
+
+	DirectedGraph graph(size,ch);
+
+	for(int i=0;i<size;i++)
+	{
+		for(int j=0;j<size;j++)
+		{
+			if(mat[i][j] == 1)
+			{
+				graph.add(i,j);
+			}
+		}
+	}
+
+	graph.print();
+
+	while(1)
+	{
+		cout<<endl;
+		cout<<"1.Add a new Edge.\n";
+		cout<<"2.Delete an edge.\n";
+		cout<<"3.Check if an edge exists.\n";
+		cout<<"4.Run bfs.\n";
+		cout<<"5.Run dfs.\n";
+		cout<<"6.Graph Info.\n";
+		cout<<"7.Exit\n";
+
+		int choice,src,des;
+		cout<<"Enter your choice : ";
+		cin>>choice;
+
+		switch(choice)
+		{
+		case 1:
+			cout<<"Enter source      : ";
+			cin>>src;
+			cout<<"Enter destination : ";
+			cin>>des;
+			graph.add(src,des);
+		break;
+		case 2:
+			cout<<"Enter source      : ";
+			cin>>src;
+			cout<<"Enter destination : ";
+			cin>>des;
+			graph.remove(src,des);
+		break;
+		case 3:
+			cout<<"Enter source      : ";
+			cin>>src;
+			cout<<"Enter destination : ";
+			cin>>des;
+			if(graph.edgeExists(src,des))
+			{
+				cout<<"YES\n";
+			}
+			else
+			{
+				cout<<"NO\n";
+			}
+		break;
+		case 4:
+			cout<<"Source : ";
+			cin>>src;
+			graph.bfs(&print,src);
+		break;
+		case 5:
+			cout<<"Source : ";
+			cin>>src;
+			graph.dfs(&print,src);
+		break;
+		case 6:
+			graph.print();
+		break;
+		case 7:
+			return 0;
+		default: 
+			cout<<"Wrong choice.\n";
+		}
+
+	}
+
 	return 0;
+}
+
+int getLines(char *file)
+{
+	string command = "wc -l ";
+	command += file;
+	command += " > output";
+	system(command.c_str());
+	ifstream infile;
+	infile.open("output");
+	int ret;
+	infile>>ret;
+	infile.close();
+	system("rm output");
+	return ret;
 }
